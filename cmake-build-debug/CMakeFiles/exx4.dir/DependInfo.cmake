c7f190f6d79541120a84e1b0e85bf2abe13d0d2d
shirsabo <shirsabo100@gmail.com>
2020-01-14 18:08:28 +0200

    first push

---
+++ cmake-build-debug/CMakeFiles/exx4.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/osboxes/CLionProjects/ex4.1/MatrixMySearch.cpp" "/home/osboxes/CLionProjects/ex4.1/cmake-build-debug/CMakeFiles/exx4.dir/MatrixMySearch.cpp.o"
+  "/home/osboxes/CLionProjects/ex4.1/MyClientHandler.cpp" "/home/osboxes/CLionProjects/ex4.1/cmake-build-debug/CMakeFiles/exx4.dir/MyClientHandler.cpp.o"
+  "/home/osboxes/CLionProjects/ex4.1/MySerialServer.cpp" "/home/osboxes/CLionProjects/ex4.1/cmake-build-debug/CMakeFiles/exx4.dir/MySerialServer.cpp.o"
+  "/home/osboxes/CLionProjects/ex4.1/MyTestClientHandler.cpp" "/home/osboxes/CLionProjects/ex4.1/cmake-build-debug/CMakeFiles/exx4.dir/MyTestClientHandler.cpp.o"
+  "/home/osboxes/CLionProjects/ex4.1/StringReverser.cpp" "/home/osboxes/CLionProjects/ex4.1/cmake-build-debug/CMakeFiles/exx4.dir/StringReverser.cpp.o"
+  "/home/osboxes/CLionProjects/ex4.1/main.cpp" "/home/osboxes/CLionProjects/ex4.1/cmake-build-debug/CMakeFiles/exx4.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
